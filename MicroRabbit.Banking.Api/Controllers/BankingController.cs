f78f53c7aab4410e3340e5c24a99b9c539e0ee2a
brainiacOnly <maxonly9@gmail.com>
2020-10-13 16:12:54 +0300

    Partially completed Banking Microservice

---
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BankingController : ControllerBase
+    {
+        private readonly IAccountService accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            this.accountService = accountService;
+        }
+        
+        [HttpGet]
+        public IEnumerable<Account> Get()
+        {
+            return accountService.GetAccounts();
+        }
+    }
+}
---
+++ MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private BankingDbContext context;
+
+        public AccountRepository(BankingDbContext context)
+        {
+            this.context = context;
+        }
+        
+        public IEnumerable<Account> GetAccounts()
+        {
+            return context.Accounts;
+        }
+    }
+}
